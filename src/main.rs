b3e277972ce30c18d4b1c840358c72a455ea2c4e
David A Burns <davidaaronburns@gmail.com>
2020-05-09 20:02:54 -0400

    Implemented the decoding of instructions and mapping them to their proper opcode index

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ impl<'a> Chip8Emulator<'a> {
     
     fn initialize(&mut self) {
         self.load_font_glyphs();
-        self.cpu.program_counter = 0x200;
+        self.cpu.initialize();
     }
 
     fn load_font_glyphs(&mut self) {
@@ -89,6 +89,7 @@ impl<'a> Chip8Emulator<'a> {
 
 struct Chip8<'a> {
     memory:  [u8; 4096], // TODO: Implement a 'Ram' struct to house the ram + read and write functionality
+    vram: [u8; 64 * 32], // TODO: Implement a 'Ram' struct to house the ram + read and write functionality
     v: [u8; 16],
     i: u16,
     delay_timer: u8,
@@ -96,12 +97,14 @@ struct Chip8<'a> {
     program_counter: usize,
     stack_pointer: u8,
     stack: [u16; 16],
-    vram: [u8; 64 * 32], // TODO: Implement a 'Ram' struct to house the ram + read and write functionality
-    instructions: [fn(&mut Chip8<'a>); 32],
+    
+    // Emulator specific
+    instructions: [fn(&mut Chip8<'a>); 36],
     opcode: u16,
+    opcode_index: usize
 }
 
-// Basic CPU functionality
+// CPU functionality
 impl<'a> Chip8<'a> {
     fn new() -> Chip8<'a> {
         Chip8 {
@@ -114,9 +117,34 @@ impl<'a> Chip8<'a> {
             stack_pointer: 0x00,
             stack: [0x0000; 16],
             vram: [0x00; 64 * 32],
-            instructions: [Chip8::noop; 32],
-            opcode: 0x0000
+            instructions: [Chip8::noop; 36],
+            opcode: 0x0000,
+            opcode_index: 0
         }
+    }
+
+    fn initialize(&mut self) {
+        self.v = [0x00; 16];
+        self.i = 0x0000;
+        self.delay_timer = 0;
+        self.sound_timer = 0;
+        self.program_counter = 0x0200;
+        self.stack_pointer = 0x00;
+        self.stack = [0x000; 16];
+        self.opcode = 0x0000;
+        self.opcode_index = 0;
+
+        self.instructions = [
+            Chip8::noop, Chip8::_0nnn, Chip8::_00e0, Chip8::_00ee,
+            Chip8::_1nnn, Chip8::_2nnn, Chip8::_3xnn, Chip8::_4xnn,
+            Chip8::_5xy0, Chip8::_6xnn, Chip8::_7xnn, Chip8::_8xy0,
+            Chip8::_8xy1, Chip8::_8xy2, Chip8::_8xy3, Chip8::_8xy4,
+            Chip8::_8xy5, Chip8::_8xy6, Chip8::_8xy7, Chip8::_8xye,
+            Chip8::_9xy0, Chip8::_annn, Chip8::_bnnn, Chip8::_cxnn,
+            Chip8::_dxyn, Chip8::_ex9e, Chip8::_exa1, Chip8::_fx07,
+            Chip8::_fx0a, Chip8::_fx15, Chip8::_fx18, Chip8::_fx1e,
+            Chip8::_fx29, Chip8::_fx33, Chip8::_fx55, Chip8::_fx65, 
+        ]
     }
 
     fn cycle(&mut self) {
@@ -125,12 +153,15 @@ impl<'a> Chip8<'a> {
         self.execute();
         self.timers();
 
-        //DEBUG PURPOSES
+        // DEBUG PURPOSES
         if (self.program_counter + 2 > 0x0FFF) {
             self.program_counter = 0x200
         } else {
             self.program_counter += 2;
-        }        
+        }
+        
+        // DEBUG PURPOSES FOR CONSOLE OUTPUT
+        print!("\n");
     }
 
     fn fetch(&mut self) {
@@ -139,11 +170,69 @@ impl<'a> Chip8<'a> {
     }
 
     fn decode(&mut self) {
-
+        match (self.opcode & 0xF000) {
+            0x0 => {
+               match (self.opcode & 0x00FF) {
+                   0x0000 => self.opcode_index = 0,
+                   0x00E0 => self.opcode_index = 2,
+                   0x00EE => self.opcode_index = 3, 
+                   _ => self.opcode_index = 1
+               } 
+            },
+            0x1000 => self.opcode_index = 4,
+            0x2000 => self.opcode_index = 5,
+            0x3000 => self.opcode_index = 6,
+            0x4000 => self.opcode_index = 7,
+            0x5000 => self.opcode_index = 8,
+            0x6000 => self.opcode_index = 9,
+            0x7000 => self.opcode_index = 10,
+            0x8000 => {
+                match (self.opcode & 0x000F) {
+                    0x0000 => self.opcode_index = 11,
+                    0x0001 => self.opcode_index = 12,
+                    0x0002 => self.opcode_index = 13,
+                    0x0003 => self.opcode_index = 14,
+                    0x0004 => self.opcode_index = 15,
+                    0x0005 => self.opcode_index = 16,
+                    0x0006 => self.opcode_index = 17,
+                    0x0007 => self.opcode_index = 18,
+                    0x000E => self.opcode_index = 19,
+                    _ => self.opcode_index = 0
+                } 
+            },
+            0x9000 => self.opcode_index = 20,
+            0xA000 => self.opcode_index = 21,
+            0xB000 => self.opcode_index = 22,
+            0xC000 => self.opcode_index = 23,
+            0xD000 => self.opcode_index = 24,
+            0xE000 => {
+                match (self.opcode & 0x00FF) {
+                    0x009E => self.opcode_index = 25,
+                    0x00A1 => self.opcode_index = 26,
+                    _ => self.opcode_index = 0
+                }
+            },
+            0xF000 => {
+                match (self.opcode & 0x00FF) {
+                    0x0007 => self.opcode_index = 27,
+                    0x000A => self.opcode_index = 28,
+                    0x0015 => self.opcode_index = 29,
+                    0x0018 => self.opcode_index = 30,
+                    0x001E => self.opcode_index = 31,
+                    0x0029 => self.opcode_index = 32,
+                    0x0033 => self.opcode_index = 33,
+                    0x0055 => self.opcode_index = 34,
+                    0x0065 => self.opcode_index = 35,
+                    _ => self.opcode_index = 0
+                }
+            },
+            _ => self.opcode_index = 0
+        }
     }
 
     fn execute(&mut self) {
-        println!("${:04x} {:04x}", self.program_counter, self.opcode);      
+        print!("${:04x} {:04x} [{}] ", self.program_counter, self.opcode, self.opcode_index);
+        self.instructions[self.opcode_index](self);      
     }
 
     fn timers(&mut self) {
@@ -154,7 +243,7 @@ impl<'a> Chip8<'a> {
         if (self.sound_timer > 0) {
             self.sound_timer -= 1;
             if (self.sound_timer == 0) {
-                println!("BEEP");
+                print!(" BEEP");
             }
         }
     }
@@ -163,147 +252,147 @@ impl<'a> Chip8<'a> {
 impl<'a> Chip8<'a> {
     fn noop(&mut self) {
         // Empty instruction for initialization purposes
-        println!("noop");
+        print!("noop");
     }
 
     fn _0nnn(&mut self) {
-        println!("_0nnn");
+        print!("_0nnn");
     }
 
     fn _00e0(&mut self) {
-        println!("_00e0");
+        print!("_00e0");
     }
 
     fn _00ee(&mut self) {
-        println!("_00ee");
+        print!("_00ee");
     }
 
     fn _1nnn(&mut self) {
-        println!("_1nnn");
+        print!("_1nnn");
     }
 
     fn _2nnn(&mut self) {
-        println!("_2nnn");
+        print!("_2nnn");
     }
 
     fn _3xnn(&mut self) {
-        println!("_3xnn");
+        print!("_3xnn");
     }
 
     fn _4xnn(&mut self) {
-        println!("_4xnn");
+        print!("_4xnn");
     }
 
     fn _5xy0(&mut self) {
-        println!("_4xnn");
+        print!("_4xnn");
     }
 
     fn _6xnn(&mut self) {
-        println!("_6xnn");
+        print!("_6xnn");
     }
 
     fn _7xnn(&mut self) {
-        println!("_7xnn");
+        print!("_7xnn");
     }
 
     fn _8xy0(&mut self) {
-        println!("_8xy0");
+        print!("_8xy0");
     }
 
     fn _8xy1(&mut self) {
-        println!("_8xy1");
+        print!("_8xy1");
     }
 
     fn _8xy2(&mut self) {
-        println!("_8xy2");
+        print!("_8xy2");
     }
 
     fn _8xy3(&mut self) {
-        println!("_8xy3");
+        print!("_8xy3");
     }
 
     fn _8xy4(&mut self) {
-        println!("_8xy4");
+        print!("_8xy4");
     }
 
     fn _8xy5(&mut self) {
-        println!("_8xy5");
+        print!("_8xy5");
     }
 
     fn _8xy6(&mut self) {
-        println!("_8xy6");
+        print!("_8xy6");
     }
 
     fn _8xy7(&mut self) {
-        println!("_8xy7");
+        print!("_8xy7");
     }
 
     fn _8xye(&mut self) {
-        println!("_8xye");
+        print!("_8xye");
     }
 
     fn _9xy0(&mut self) {
-        println!("_9xy0");
+        print!("_9xy0");
     }
 
     fn _annn(&mut self) {
-        println!("_annn");
+        print!("_annn");
     }
 
     fn _bnnn(&mut self) {
-        println!("_bnnn");
+        print!("_bnnn");
     }
 
     fn _cxnn(&mut self) {
-        println!("_cxnn");
+        print!("_cxnn");
     }
 
     fn _dxyn(&mut self) {
-        println!("_dxyn");
+        print!("_dxyn");
     }
 
     fn _ex9e(&mut self) {
-        println!("_ex9e");
+        print!("_ex9e");
     }
 
     fn _exa1(&mut self) {
-        println!("_exa1");
+        print!("_exa1");
     }
 
     fn _fx07(&mut self) {
-        println!("_fx07");
+        print!("_fx07");
     }
 
     fn _fx0a(&mut self) {
-        println!("_fx0a");
+        print!("_fx0a");
     }
 
     fn _fx15(&mut self) {
-        println!("_fx15");
+        print!("_fx15");
     }
 
     fn _fx18(&mut self) {
-        println!("_fx18");
+        print!("_fx18");
     }
 
     fn _fx1e(&mut self) {
-        println!("_fx1e");
+        print!("_fx1e");
     }
 
     fn _fx29(&mut self) {
-        println!("_fx29");
+        print!("_fx29");
     }
 
     fn _fx33(&mut self) {
-        println!("_fx33");
+        print!("_fx33");
     }
 
     fn _fx55(&mut self) {
-        println!("_fx55");
+        print!("_fx55");
     }
 
     fn _fx65(&mut self) {
-        println!("_fx65");
+        print!("_fx65");
     }
 }
 
